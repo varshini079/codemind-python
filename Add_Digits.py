be5f020c3ca7f70bbc1aa1b287d9e66f14161e1e
varshini079 <104311746+varshini079@users.noreply.github.com>
2022-06-11 03:30:04 +0530

    def sum(n):     s=0     while (n>0 or s>9):         if(n==0):             n=s             s=0         s=s+n%10         n=n//10     return(s) n=int(input()) t=sum(n) print(t)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,12 +1,12 @@
 def sum(n):
-    s= 0
-    while(n>0or s>9):
+    s=0
+    while (n>0 or s>9):
         if(n==0):
             n=s
             s=0
-        s+=n%10
-        n//=10
-    return s
-num=int(input())
-r=sum(num)
-print(r)
+        s=s+n%10
+        n=n//10
+    return(s)
+n=int(input())
+t=sum(n)
+print(t)
