cff9e632e90d15a90d910a97937b45117957b0de
varshini079 <104311746+varshini079@users.noreply.github.com>
2022-06-11 04:05:21 +0530

    def hn(n):     s=0     while(n>0):         r=n%10         s=s+(r)**2         n=n//10     return(s) n=int(input()) t=len(str(n)) while(t>=2):     n2=hn(n)     n=n2     t=len(str(n)) if(n2==1 or n2==7):     print(True) else:     print(False)

--- happy_number.py
+++ happy_number.py
@@ -1,15 +1,20 @@
-def hn(num):
-    r=s=0
-    while num>0:
-        r=num%10
-        s=s+(r*r)
-        num=num//10
-    return s
-num=int(input())
-n1=num
-while(n1!=1 and n1!=4):
-    n1=hn(n1)
-if n1==1 or n1==7:
+def hn(n):
+    s=0
+    while(n>0):
+        r=n%10
+        s=s+(r)**2
+        n=n//10
+    return(s)
+n=int(input())
+t=len(str(n))
+while(t>=2):
+    n2=hn(n)
+    n=n2
+    t=len(str(n))
+if(n2==1 or n2==7):
     print(True)
 else:
     print(False)
+    
+    
+    
