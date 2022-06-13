adfafc3cd5f05654700605b5606830bf7b22ace6
varshini079 <104311746+varshini079@users.noreply.github.com>
2022-06-13 05:47:19 +0530

    def prime(n):     if n>1:         for i in range(2,int(n**0.5)+1):             if n%i==0:                 return(False)         else:             return(True)
    j=int(input())
    for i in range(j):
        if prime(j-i) and prime(j+i):
            print(i)
            break
        elif prime(j-i):
            print(i)
            break
        elif prime(j+i):
            print(i)
            break

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -0,0 +1,23 @@
+def prime(n):
+    if n>1:
+        for i in range(2,int(n**0.5)+1):
+            if n%i==0:
+                return(False)
+        else:
+            return(True)
+
+j=int(input())
+for i in range(j):
+    if prime(j-i) and prime(j+i):
+        print(i)
+        break
+    elif prime(j-i):
+        print(i)
+        break
+    elif prime(j+i):
+        print(i)
+        break
+        
+        
+        
+    
